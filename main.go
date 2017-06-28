14df78efe2a89daafb45beae54497b70ee43173f
Juan Ignacio Rizza <juan.rizza@gmail.com>
2017-06-27 22:07:36 -0300

    Fixed problems with struts and pointers

--- main.go
+++ main.go
@@ -3,10 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+//Show type
+type Show struct {
+	ID        int
+	name      string
+	timeTable []string
+}
+
+//Place type
+type Place struct {
+	ID      int
+	Name    string
+	Address string
+	Shows   []Show
+}
 
 func main() {
 
@@ -20,18 +36,26 @@ func main() {
 	fmt.Println("Fetching Cinemas from here: ", cinesURL)
 	fmt.Println("Fetching Theaters from here: ", teatrosURL)
 
-	//getPlaces(cinesURL, cineURL, "cine")
-	//getPlaces(teatrosURL, obraURL, "teatro")
+	places := make([]Place, 0, 2)
 
-	getDetails("432", "Sunstar Cinemas Bariloche", "cine", "http://www.123info.com.ar/salacine/")
-	getDetails("317", "Actors Studio", "teatro", "http://www.123info.com.ar/salateatro/")
+	err := getPlaces(cinesURL, cineURL, "cine", &places)
+	if err != nil {
+		log.Printf("Error....")
+	}
+	for i, p := range places {
+		log.Printf("Place %d: %s", i, p.Name)
+		for j, s := range p.Shows {
+			log.Printf("Show %d: %s", j, s.name)
+		}
+	}
+	//getPlaces(teatrosURL, obraURL, "teatro")
 
 }
 
-func getPlaces(mainURL, detailsURL, kind string) {
+func getPlaces(mainURL, detailsURL, kind string, places *[]Place) error {
 	doc, err := goquery.NewDocument(mainURL)
 	if err != nil {
-		log.Fatal("Error", err)
+		log.Fatal("error", err)
 	}
 
 	var path = "#" + kind + " option"
@@ -42,39 +66,55 @@ func getPlaces(mainURL, detailsURL, kind string) {
 		if !ok {
 			log.Printf("value is not present")
 		}
-		name := s.Text()
-		getDetails(id, name, kind, detailsURL)
-
+		newID, err := strconv.Atoi(id)
+		if err != nil {
+			log.Printf("Cannot convert")
+		}
+		if newID == 760 {
+			name := s.Text()
+			lugar := new(Place)
+			lugar.ID = newID
+			lugar.Name = name
+			lugar.Shows, err = getDetails(id, name, kind, detailsURL)
+			if err != nil {
+				log.Printf("error")
+			}
+			*places = append(*places, *lugar)
+		}
 	})
+	return nil
 }
 
-func getDetails(id, name, kind, detailsURL string) {
-	fmt.Printf("Place (%s) with ID: %s and Name: %s\n", strings.Title(kind), id, name)
+func getDetails(id, name, kind, detailsURL string) ([]Show, error) {
 
 	docDetails, err := goquery.NewDocument(detailsURL + id)
 	if err != nil {
-		log.Fatal("Error", err)
+		return nil, err
 	}
-	direccion := docDetails.Find(".BusquedaResultado").Text()
-	direccion = standardizeSpaces(direccion)
-	direccion = strings.Replace(direccion, name, name+",", -1)
+	address := docDetails.Find(".BusquedaResultado").Text()
+	address = standardizeSpaces(address)
+	address = strings.Replace(address, name, name+",", -1)
 
-	fmt.Printf("Direccion: %q\n", direccion)
+	show := new(Show)
+	shows := make([]Show, 0, 2)
 
 	docDetails.Find("h3").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
-		movie := s.Find(".azul").Text()
-		timeTable := s.Find(".Desp_DosColBusquedaB.linh16").Text()
+		name := s.Find(".azul").Text()
+		if name != "" {
+			timeTable := s.Find(".Desp_DosColBusquedaB.linh16").Text()
+			timeTable = standardizeSpaces(timeTable)
+			timeTable = strings.TrimPrefix(timeTable, "Horarios:")
+			timeTable = strings.Replace(timeTable, ".", ",", -1)
+			results := strings.Split(timeTable, ",")
+			results = deleteEmpty(results)
+			show.name = name
+			show.timeTable = results
 
-		timeTable = standardizeSpaces(timeTable)
-		timeTable = strings.TrimPrefix(timeTable, "Horarios:")
-		timeTable = strings.Replace(timeTable, ".", ",", -1)
-		results := strings.Split(timeTable, ",")
-		results = deleteEmpty(results)
-		if movie != "" {
-			fmt.Printf("Movie: %s - Times: %v\n", movie, results)
+			shows = append(shows, *show)
 		}
 	})
+	return shows, nil
 }
 
 func deleteEmpty(s []string) []string {
