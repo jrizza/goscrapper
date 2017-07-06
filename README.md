9b2f2189e8e31e8f4e21fa1806b2157302492dd7
Juan Ignacio Rizza <juan.rizza@gmail.com>
2017-07-06 15:11:05 -0300

    Added concurrency, generate json files

---
+++ README.md
@@ -0,0 +1,10 @@
+# goscrapper
+
+First Go project used to web scrappe Cinemas and Theaters information of Argentina.
+
+Pending items:
+
+- Get geolocalization of each cinema
+- Store json data
+- Update json data
+- Front-End
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
-	"strconv"
+	"os"
 	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -18,7 +20,7 @@ type Show struct {
 
 //Place type
 type Place struct {
-	ID      int
+	ID      string
 	Name    string
 	Address string
 	Shows   []Show
@@ -33,27 +35,30 @@ func main() {
 		obraURL    = "http://www.123info.com.ar/salateatro/"
 	)
 
+	var wg sync.WaitGroup
+
 	fmt.Println("Fetching Cinemas from here: ", cinesURL)
 	fmt.Println("Fetching Theaters from here: ", teatrosURL)
 
-	cines := make([]Place, 0, 2)
-	teatros := make([]Place, 0, 2)
+	cines := make([]Place, 0, 100)
+	teatros := make([]Place, 0, 50)
 
-	err := getPlaces(cinesURL, cineURL, "cine", &cines)
-	if err != nil {
-		log.Printf("error getting places: %v", err)
-	}
-	err = getPlaces(teatrosURL, obraURL, "teatro", &teatros)
-	if err != nil {
+	if err := fetchPlaces(cinesURL, cineURL, "cine", &cines, &wg); err != nil {
 		log.Printf("error getting places: %v", err)
 	}
 
-	// for i, p := range places {
-	// 	log.Printf("Place %d: %s", i, p.Name)
-	// 	for j, s := range p.Shows {
-	// 		log.Printf("Show %d: %s", j, s.Name)
-	// 	}
-	// }
+	if err := fetchPlaces(teatrosURL, obraURL, "teatro", &teatros, &wg); err != nil {
+		log.Printf("error getting places: %v", err)
+	}
+
+	wg.Wait()
+
+	for i, p := range cines {
+		log.Printf("Place %d: %s - %s", i, p.Name, p.Address)
+		for j, s := range p.Shows {
+			log.Printf("Show %d: %s", j, s.Name)
+		}
+	}
 	c, err := json.Marshal(cines)
 	if err != nil {
 		log.Printf("error converting to json: %v", err)
@@ -63,69 +68,75 @@ func main() {
 		log.Printf("error converting to json: %v", err)
 	}
 
-	log.Println(string(c))
-	log.Println(string(t))
+	fc, err := os.Create("cine.json")
+	ft, err := os.Create("teatro.json")
+	defer fc.Close()
+	defer ft.Close()
+
+	wc := bufio.NewWriter(fc)
+	wt := bufio.NewWriter(ft)
+	fmt.Fprint(wc, string(c))
+	fmt.Fprint(wt, string(t))
+	// log.Sprint(string(c))
+	// log.Println(string(t))
 }
 
-func getPlaces(mainURL, detailsURL, kind string, places *[]Place) error {
+func fetchPlaces(mainURL, detailsURL, kind string, places *[]Place, wg *sync.WaitGroup) error {
+
 	doc, err := goquery.NewDocument(mainURL)
 	if err != nil {
 		log.Fatal("error", err)
+		return err
 	}
 
-	var path = "#" + kind + " option"
+	path := "#" + kind + " option"
 
 	doc.Find(path).Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
+
 		id, ok := s.Attr("value")
 		if !ok {
 			log.Printf("value is not present")
 		}
-		newID, err := strconv.Atoi(id)
-		if err != nil {
-			log.Printf("cannot convert string id: %v", err)
-		}
-		if newID == 760 || newID == 485 {
-			lugar := new(Place)
-			lugar.ID = newID
-			lugar.Name = s.Text()
-			err = getDetails(id, lugar.Name, kind, detailsURL, lugar)
-			if err != nil {
-				log.Printf("error")
-			}
-			*places = append(*places, *lugar)
-		}
-	})
-	return nil
-}
+		//newID, err := strconv.Atoi(id)
+		//if err != nil {
+		//	log.Printf("cannot convert string id: %v", err)
+		//}
+		//Hardcoded for testing purpose
+		if id != "0" {
+			go func() {
+				wg.Add(1)
+				defer wg.Done()
+				place := new(Place)
+				place.ID = id
+				place.Name = s.Text()
+				docDetails, err := goquery.NewDocument(detailsURL + id)
+				if err != nil {
+					return
+				}
+				address := docDetails.Find(".BusquedaResultado").Text()
+				address = standardizeSpaces(address)
+				address = strings.Replace(address, place.Name, place.Name+",", -1)
+				place.Address = address
 
-func getDetails(id, name, kind, detailsURL string, lugar *Place) error {
+				docDetails.Find("h3").Each(func(i int, s *goquery.Selection) {
+					name := s.Find(".azul").Text()
+					if name != "" {
+						timeTable := s.Find(".Desp_DosColBusquedaB.linh16").Text()
+						timeTable = standardizeSpaces(timeTable)
+						timeTable = strings.TrimPrefix(timeTable, "Horarios:")
+						timeTable = strings.Replace(timeTable, ".", ",", -1)
+						results := strings.Split(timeTable, ",")
+						results = deleteEmpty(results)
 
-	docDetails, err := goquery.NewDocument(detailsURL + id)
-	if err != nil {
-		return err
-	}
-	address := docDetails.Find(".BusquedaResultado").Text()
-	address = standardizeSpaces(address)
-	address = strings.Replace(address, name, name+",", -1)
-	lugar.Address = address
-
-	docDetails.Find("h3").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
-		name := s.Find(".azul").Text()
-		if name != "" {
-			timeTable := s.Find(".Desp_DosColBusquedaB.linh16").Text()
-			timeTable = standardizeSpaces(timeTable)
-			timeTable = strings.TrimPrefix(timeTable, "Horarios:")
-			timeTable = strings.Replace(timeTable, ".", ",", -1)
-			results := strings.Split(timeTable, ",")
-			results = deleteEmpty(results)
-			//show.name = name
-			//show.timeTable = results
-			lugar.Shows = append(lugar.Shows, Show{Name: name, Timetable: results})
+						place.Shows = append(place.Shows, Show{Name: name, Timetable: results})
+					}
+				})
+				log.Printf("... %s .......", place.Name)
+				*places = append(*places, *place)
+			}()
+			//log.Printf("address: %p", &place)
 		}
 	})
-
 	return nil
 }
 
